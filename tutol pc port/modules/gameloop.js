425188c6a007b557bbb3a1d32496a1b4a6407ea5
smeh8 <baronereisen@gmail.com>
2025-09-26 13:23:55 -0400

    trying to implement L4 correctly
    you're a poopyhead

--- tutol pc port/modules/gameloop.js
+++ tutol pc port/modules/gameloop.js
@@ -15,12 +15,23 @@ var gameData = {
 
 const leafUpgradeCost = {
     LU2: 10,
-    LU3: 35
+    LU3: 35,
+    LU4: 150
 }
+
+var leafUpgradeFactor = {
+    L4: 1,
+    L4Bought: false,
+    L4AtUpgradeBought: 1,
+    L4Amt: 10
+
+}
+
 
 document.getElementById("L1").disabled = false;
 document.getElementById("L2").disabled = false;
 document.getElementById("L3").disabled = false;
+document.getElementById("L4").disabled = false;
 
 console.log(`on startup, gameData.gameData.leavesPerTick is ${gameData.leavesPerTick}`);
 console.log(`on startup, gameData.tickSpeedMultiplier is ${gameData.tickSpeedMultiplier}`);
@@ -36,6 +47,15 @@ function gameLoop() {
   gameData.treeAge += deltaTime;
   gameData.treeAgePerTick = gameData.tickSpeedMultiplier;
   
+  /* if (leafUpgradeFactor.L4Bought == true) {
+    leafUpgradeFactor.L4 = (((gameData.treeAge / leafUpgradeFactor.L4Amt) ** 0.5) + 1);
+    var deltaLUFL4 = leafUpgradeFactor.L4 - leafUpgradeFactor.L4AtUpgradeBought;
+    gameData.leavesPerTick *= leafUpgradeFactor.L4;
+    gameData.leavesPerTick + deltaLUFL4.L4;
+    leafUpgradeFactor.L4AtUpgradeBought = leafUpgradeFactor.L4;
+    document.getElementById("L4").innerHTML = `L4 <br> Develop I (Bought) <br> Tree Age boosts Leaves <br> Cost: ` + leafUpgradeCost.LU4 + ` Leaves <br> Effect: ${truncateToDecimalPlaces(leafUpgradeFactor.L4, 3)}x`;
+  } */
+
   document.getElementById("pleaseWork").innerHTML = truncateToDecimalPlaces(gameData.leaves, 3) + " Leaves";
   document.getElementById("leavesPerSecond").innerHTML = truncateToDecimalPlaces(gameData.leavesPerTick, 3) + " Leaves/s";
   document.getElementById("treeAgeCounter").innerHTML = truncateToDecimalPlaces((gameData.treeAge / 1000), 3) + " Seconds";
@@ -63,6 +83,7 @@ function startGeneration() {
   gameData.leavesPerTick = 1;
   gameData.tickSpeedMultiplier = 1;
   gameData.treeAgePerTick = 1;
+  leafUpgradeFactor.L4Bought = false;
 
   document.getElementById("L1").disabled = true;
   console.log(`startGeneration function called, value of gameData.gameStarted is ${gameData.gameStarted}`);
@@ -100,6 +121,19 @@ function L3() {
     document.getElementById("L3").innerHTML = `L3 <br> Grow II (Bought) <br> x3 Leaves <br> Cost: ` + leafUpgradeCost.LU3 + ` Leaves`;
     document.getElementById("L3").disabled = true;
     console.log(`L3 is bought, gameData.leavesPerTick is now ${gameData.leavesPerTick}`);
+    }
+}
+function L4() {
+    if (gameData.leaves >= leafUpgradeCost.LU4) {
+    gameData.leaves -= leafUpgradeCost.LU4;
+    leafUpgradeFactor.L4 = (((gameData.treeAge / leafUpgradeFactor.L4Amt) ** 0.5) + 1);
+    gameData.leavesPerTick *= leafUpgradeFactor.L4;
+    document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
+    document.getElementById("L4").innerHTML = `L4 <br> Develop I (Bought) <br> Tree Age boosts Leaves <br> Cost: ` + leafUpgradeCost.LU4 + ` Leaves <br> Effect: ${truncateToDecimalPlaces(leafUpgradeFactor.L4, 3)}x`;
+    document.getElementById("L4").disabled = true;
+    console.log(`L4 is bought, gameData.leavesPerTick is now ${gameData.leavesPerTick}`);
+    leafUpgradeFactor.L4AtUpgradeBought = leafUpgradeFactor.L4;
+    leafUpgradeFactor.L4Bought = true;
     }
 }
 
