3c4c727128303cfe476def72b630f58fd3a5f7e9
smeh8 <baronereisen@gmail.com>
2025-09-27 00:44:40 -0400

    L10
    -Added L10 using my L4 function
    //god I have to make a function builder for this

--- tutol pc port/src/gameloop.js
+++ tutol pc port/src/gameloop.js
@@ -11,6 +11,7 @@
  treeAge: 0,
  treeAgePerTick: 0,
  gameStarted: false,
+ leafUpgradeCounter: 0,
 
  refreshRate: 40
 }
@@ -23,15 +24,18 @@ const leafUpgradeCost = {
     LU6: 1500,
     LU7: 5000,
     LU8: 7500,
-    LU9: 24000
+    LU9: 24000,
+    LU10: 200000
 }
 
 var leafUpgradeFactor = {
     L4: 1,
     L4Bought: false,
     L4AtUpgradeBought: 1,
-    L4Amt: 10
-
+    L4Amt: 10,
+    L10: 1,
+    L10Bought: false,
+    L10AtUpgradeBought: 1
 }
 
 
@@ -44,6 +48,7 @@ document.getElementById("L6").disabled = false;
 document.getElementById("L7").disabled = false;
 document.getElementById("L8").disabled = false;
 document.getElementById("L9").disabled = false;
+document.getElementById("L10").disabled = false;
 
 document.getElementById("refreshRate").textContent = gameData.refreshRate;
 
@@ -58,6 +63,7 @@ function gameLoop() {
   const ticksToProcess = (deltaTime / 1000) * gameData.tickSpeedMultiplier;
   
   L4UpgradeUpdater();
+  L10UpgradeUpdater();
 
   gameData.leaves += gameData.leavesPerTick * ticksToProcess;
   gameData.treeAge += ((deltaTime * gameData.tickSpeedMultiplier) / 2);
@@ -100,6 +106,7 @@ function startGeneration() {
   gameData.leavesPerTick = 1;
   gameData.tickSpeedMultiplier = 1;
   gameData.treeAgePerTick = 1;
+  gameData.leafUpgradeCounter = 1;
   leafUpgradeFactor.L4Bought = false;
 
   document.getElementById("L1").disabled = true;
@@ -124,6 +131,7 @@ function L2() {
     if (gameData.leaves >= leafUpgradeCost.LU2) {
     gameData.leaves -= leafUpgradeCost.LU2;
     gameData.leavesPerTick *= 2;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L2").innerHTML = `L2 <br> Grow I (Bought) <br> x2 Leaves <br> Cost: ` + leafUpgradeCost.LU2 + ` Leaves`;
     document.getElementById("L2").disabled = true;
@@ -134,6 +142,7 @@ function L3() {
     if (gameData.leaves >= leafUpgradeCost.LU3) {
     gameData.leaves -= leafUpgradeCost.LU3;
     gameData.leavesPerTick *= 3;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L3").innerHTML = `L3 <br> Grow II (Bought) <br> x3 Leaves <br> Cost: ` + leafUpgradeCost.LU3 + ` Leaves`;
     document.getElementById("L3").disabled = true;
@@ -155,6 +164,7 @@ function L4() {
         }
         if (leafUpgradeFactor.L4Bought == true) {
             L4UpgradeFormula();
+            gameData.leafUpgradeCounter += 1;
             gameData.leavesPerTick *= leafUpgradeFactor.L4;
             document.getElementById("L4").innerHTML = `L4 <br> Develop I (Bought) <br> Tree Age boosts Leaves <br> Cost: ` + leafUpgradeCost.LU4 + ` Leaves <br> Effect: ${truncateToDecimalPlaces(leafUpgradeFactor.L4, 3)}x`;
             console.log(`L4 is bought, gameData.leavesPerTick is now ${gameData.leavesPerTick}`);
@@ -182,6 +192,7 @@ function L5() {
     if (gameData.leaves >= leafUpgradeCost.LU5) {
     gameData.leaves -= leafUpgradeCost.LU5;
     gameData.leavesPerTick *= 2.5;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L5").innerHTML = `L5 <br> Grow III (Bought) <br> x2.5 Leaves <br> Cost: ` + leafUpgradeCost.LU5 + ` Leaves`;
     document.getElementById("L5").disabled = true;
@@ -192,6 +203,7 @@ function L6() {
     if (gameData.leaves >= leafUpgradeCost.LU6) {
     gameData.leaves -= leafUpgradeCost.LU6;
     gameData.leavesPerTick *= 3;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L6").innerHTML = `L6 <br> Grow IV (Bought) <br> x3 Leaves <br> Cost: ` + leafUpgradeCost.LU6 + ` Leaves`;
     document.getElementById("L6").disabled = true;
@@ -202,6 +214,7 @@ function L7() {
     if (gameData.leaves >= leafUpgradeCost.LU7) {
     gameData.leaves -= leafUpgradeCost.LU7;
     gameData.leavesPerTick *= 3.14;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L7").innerHTML = `L5 <br> Grow V (Bought) <br> xpi Leaves for no reason <br> Cost: ` + leafUpgradeCost.LU7 + ` Leaves`;
     document.getElementById("L7").disabled = true;
@@ -212,6 +225,7 @@ function L8() {
     if (gameData.leaves >= leafUpgradeCost.LU8) {
     gameData.leaves -= leafUpgradeCost.LU8;
     gameData.leavesPerTick *= 1.75;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L8").innerHTML = `L8 <br> Grow VI (Bought) <br> x1.75 Leaves <br> Cost: ` + leafUpgradeCost.LU8 + ` Leaves`;
     document.getElementById("L8").disabled = true;
@@ -222,10 +236,42 @@ function L9() {
     if (gameData.leaves >= leafUpgradeCost.LU9) {
     gameData.leaves -= leafUpgradeCost.LU9;
     leafUpgradeFactor.L4Amt = 4;
+    gameData.leafUpgradeCounter += 1;
     document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
     document.getElementById("L9").innerHTML = `L9 <br> Develop II (Bought) <br> Increase L4's Boost <br> Cost: ` + leafUpgradeCost.LU9 + ` Leaves <br> Effect: 2.5x`;
     document.getElementById("L9").disabled = true;
     console.log(`L8 is bought, gameData.leavesPerTick is now ${gameData.leavesPerTick}`);
     }
+}
+
+function L10UpgradeFormula() {
+  leafUpgradeFactor.L10 = 1.1 ** gameData.leafUpgradeCounter;
+}
+function L10() {
+    if (gameData.leaves >= leafUpgradeCost.LU10) {
+        while (leafUpgradeFactor.L10Bought == false) {
+            gameData.leaves -= leafUpgradeCost.LU10;
+            document.getElementById("pleaseWork").innerHTML = gameData.leaves + " Leaves";
+            document.getElementById("L10").disabled = true;
+            leafUpgradeFactor.L10Bought = true;
+        }
+        if (leafUpgradeFactor.L10Bought == true) {
+            gameData.leafUpgradeCounter += 1;
+            L10UpgradeFormula();
+            gameData.leavesPerTick *= leafUpgradeFactor.L10;
+            document.getElementById("L10").innerHTML = `L10 <br> Grow Power (Bought) <br> Every Leaf Upgrade Bought Multiplies Leaves by 1.1 <br> Cost: ` + leafUpgradeCost.LU10 + ` Leaves <br> Effect: ${truncateToDecimalPlaces(leafUpgradeFactor.L10, 3)}x`;
+            console.log(`L10 is bought, gameData.leavesPerTick is now ${gameData.leavesPerTick}`);
+            leafUpgradeFactor.L10AtUpgradeBought = leafUpgradeFactor.L10;
+        }
+    }
+}
+function L10UpgradeUpdater() {
+  if (leafUpgradeFactor.L4Bought == true) {
+  setTimeout(L10UpgradeFormula, (gameData.refreshRate * 2));
+  const deltaL10U = leafUpgradeFactor.L10 - leafUpgradeFactor.L10AtUpgradeBought;
+  gameData.leavesPerTick *= (deltaL10U + 1);
+  document.getElementById("L10").innerHTML = `L10 <br> Grow Power (Bought) <br> Every LU Bought <br> Multiplies Leaves by 1.1 <br> Cost: ` + leafUpgradeCost.LU10 + ` Leaves <br> Effect: ${truncateToDecimalPlaces(leafUpgradeFactor.L10, 3)}x`;
+  leafUpgradeFactor.L10AtUpgradeBought = leafUpgradeFactor.L10;
+  }
 }
 gameLoop();
